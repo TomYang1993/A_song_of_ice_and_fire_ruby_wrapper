6557d7c89b4e836ba35333d6abe60a23feb34174
Tom Yang <xy2286@columbia.edu>
2016-09-13 20:59:33 -0400

    add 10 houses and findBy

--- lib/GOT_wrapper.rb
+++ lib/GOT_wrapper.rb
@@ -1,8 +1,33 @@
 require "GOT_wrapper/version"
+require "unirest"
 
 module GOTWrapper
+  class House
+  attr_reader :name, :region, :words
+    def initialize(house)
+      @name = house["name"]
+      @region = house["region"]
+      @words = house["words"]
+    end
 
-  class Character
+    def self.all
+        houses_array = Unirest.get("http://www.anapioficeandfire.com/api/houses?").body
+        houses = []
+        houses_array.each do |house|
+          houses << House.new(house)
+        end
+        houses
+    end
+
+    def self.find_by(house_name)
+        key = house_name.keys.first
+        value = house_name.values.first
+        house = Unirest.get("http://www.anapioficeandfire.com/api/houses?#{key}=#{value}").body
+        House.new(house)
+    end
+
+
+
 
   end
 end
